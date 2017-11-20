32121390a73214dd62585b0a976259a1290ff5dd
Wadson Fourrien <fourwadsmart@hotmail.com>
2017-11-20 19:16:03 -0400

    add changes from graph data structure

--- javascript_graphs_ds.js
+++ javascript_graphs_ds.js
@@ -21,8 +21,18 @@ function Graph(v) {
     
     this.addEdge = addEdge;
     this.toString = toString;
-    
     this.showGraph = showGraph;
+    
+    this.dfs = dfs;
+    this.bfs = bfs;
+    this.marked = [];
+    
+    for (var i = 0; i < this.vertices; ++i) {
+        this.marked[i] = false;
+    }
+    
+    this.pathTo = pathTo;
+    this.hasPathTo = hasPathTo;
     
 }
 
@@ -49,6 +59,69 @@ function showGraph() {
             }
         }
     }
+}
+
+
+/* ==============================================
+        Graph Search Algorithm: Depth First
+   ============================================== */
+
+// Depth First Search
+function dfs(v) {
+    this.marked[v] = true;
+    // if statement for print is not required
+    if (this.adj[v] != undefined) {
+        console.log("Visited vertex: " + v);
+    }
+    
+    for(let w in this.adj[v]) {
+        if(!this.marked[w]) {
+            this.dfs(w);
+        }
+    }
+}
+
+
+
+// Breadth First Search
+function bfs(s) {
+    let queue = [];
+    this.marked[s] = true;
+    queue.push(s); // add to back of queue
+    while (queue.length > 0) {
+        let v = queue.shift();
+        if (v == undefined) {
+            console.log("visited vertex: " + v);
+        }
+        for(let w in this.adj[v]) {
+            if (!this.marked[w]) {
+                this.edgeTo[w] = v;
+                this.marked[w] = true;
+                queue.push(w);
+            }
+        }
+    }
+} 
+
+
+
+// findest shortest path function
+function pathTo(v) {
+    let source = 0;
+    if (!this.hasPathTo(v)) {
+        return undefined;
+    }
+    let path = [];
+    
+    for (let i = v; i != source; i = this.edgeTo[i]) {
+        path.push(i);
+    }
+    path.push(s);
+    return path;
+}
+
+function hasPathTo(v) {
+    return this.marked[v];
 }
 
 
