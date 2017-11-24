36c2e79bf001d7fe7248e0b588a07aad3f32ef94
Wadson Fourrien <fourwadsmart@hotmail.com>
2017-11-23 20:56:06 -0400

    add basic sorting algorithm: bubblesort, insertionsort, selectionsort

--- javascript_sorting_algos.js
+++ javascript_sorting_algos.js
@@ -15,6 +15,8 @@ function CArray(numElements) {
     this.setData = setData;
     this.swap = swap;
     this.bubbleSort = bubbleSort;
+    this.selectionSort = selectionSort;
+    this.insertionSort = insertionSort;
     
     for (let i = 0; i < numElements; i++) {
         this.dataStore[i] = i;
@@ -58,7 +60,7 @@ function swap(arr, index1, index2) {
 
 
 /**
-    Bubble sort in action **
+    Bubble Sort in action **
 **/
 
 function bubbleSort() {
@@ -70,6 +72,84 @@ function bubbleSort() {
                 swap (this.dataStore, inner, inner+1);
             }
         }
+        console.log(this.toString());
     }
 }
+
+
+/**
+    Selection Sort in action **
+**/
+
+function selectionSort() {
+    let min, temp;
+    for (let outer = 0; outer <= this.dataStore.length-2; outer++) {
+        min = outer;
+        for (let inner = outer + 1; 
+             inner <= this.dataStore.length-1; inner++) {
+            if (this.dataStore[inner] < this.dataStore[min]) {
+                min = inner;
+            }
+            
+        }
+        this.swap(this.dataStore, outer, min);
+    }
+}
+
+
+
+/**
+    Insertion Sort in action **
+**/
+
+function insertionSort() {
+    let temp, inner;
+    for (let outer = 1; outer <= this.dataStore.length-1; outer++) {
+        temp = this.dataStore[outer];
+        inner = outer;
+        while (inner > 0 && (this.dataStore[inner-1] >= temp)) {
+            this.dataStore[inner] = this.dataStore[inner-1];
+            inner--;
+        }
+        this.dataStore[inner] = temp;
+    }
+}
+
+
+
+
+/********************************************************************/
+/* Snippet of code to test the runtime of the three algorithm above */
+
+var numElements = 100;
+var nums = new CArray(numElements);
+nums.setData();
+var start = new Date().getTime();
+nums.bubbleSort();
+var stop = new Date().getTime();
+var elapsed = stop - start;
+console.log("Elapsed time for the bubble sort on " +
+numElements + " elements is: " + elapsed + " milliseconds.");
+start = new Date().getTime();
+nums.selectionSort();
+stop = new Date().getTime();
+elapsed = stop - start;
+console.log("Elapsed time for the selection sort on " +
+numElements + " elements is: " + elapsed + " milliseconds.");
+start = new Date().getTime();
+nums.insertionSort();
+stop = new Date().getTime();
+elapsed = stop - start;
+console.log("Elapsed time for the insertion sort on " + 
+numElements + " elements is: " + elapsed + " milliseconds.");
+
+/********************************************************************/
+
+
+
+/**
+    Insertion Sort in action **
+**/
+
+
 
